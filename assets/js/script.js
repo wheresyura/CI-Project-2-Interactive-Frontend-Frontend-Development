2f1d3f682f40fdb9e53a6d10ae98cd1b5f5740d0
wheresyura <yurakovabusiness@gmail.com>
2021-04-10 22:38:42 +0000

    added two filters to the select bootstrap labels

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,14 @@
 //created a constant with the api URL and variables with desired exchanges
 const exchURL = "https://api.coingecko.com/api/v3/exchanges/";
 let coins = { binance: [], gdax: [], kraken: [], hotbit: [] };
+let filtered = [];
+
 
 //looping through all of the pages code to show information, (hardcoded)
 function getData2(exch) {
     coins[exch] = [];
+    filtered = [];
+
     for (let i = 1; i < 3; i++) {
         const url = exchURL + exch + `/tickers?page=${i}`;
         processRequest(exch, url);
@@ -29,21 +33,18 @@ function processRequest(exch, url){
 }
 
 // let arrivedData = function () {} // same as below
-function arrivedData() {
+function displayFilteredData(exch) {
     let result = '';
-    for (const exch in coins) {
-        // if (!exch.hasOwnProperty('tickers')) {
-        //     continue;
-        // }
+    for (const i in filtered) {
         // K - because we made the data into an array rather then a dictionary earlier we removed .tickers (which would have given us this array)
-        let tickers = coins[exch];
-        for (let i in tickers) {
-            result += `<tr><td>${exch}</td> <td>${tickers[i].base}</td> <td>${tickers[i].target}</td><td>${tickers[i].last}</td><td>${tickers[i].volume}</td></tr>`;
+       
+        
+            result += `<tr><td>${exch}</td> <td>${filtered[i].base}</td> <td>${filtered[i].target}</td><td>${filtered[i].last}</td><td>${filtered[i].volume}</td></tr>`;
             //result = result +  // the same
-        }
+       
     }
     //console.log(result); 
-    let el = document.getElementById('test');
+    let el = document.getElementById('data-table');
     el.innerHTML = `<table class='table table-dark'>${result}</table>`;
 }
 
@@ -68,28 +69,41 @@ function filterUSDT(obj) {
 // setTimeout( ()=> filterUSDT(coins['binance']), 5000);
 //console.log(filterUSDT(coins['binance']));
 
-
+// note
 
 function refreshTickers(){
     for (const exch in coins) {
         getData2(exch);
-      
     }
 }
 
 
+function filterData() {
+    let exchange = document.getElementById('exchange').value;
+    let currency = document.getElementById('currency').value;
+
+    filtered = coins[exchange].filter(ticker => ticker.target === currency);
+
+    displayFilteredData(exchange);
+
+
+
+
+
+
+};
 
 window.onload = function(e) {
     let saved = localStorage.getItem('coins');
     if (saved == null) {
         console.log('requesting data');
-        
         refreshTickers();
     }
     else {
         console.log('using saved data');
         coins = JSON.parse(saved);
-
     }
+    filterData();
+
 };
 
