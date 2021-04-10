aec37dceea59412d61080c9f5bf33fa9bc3c19b6
wheresyura <yurakovabusiness@gmail.com>
2021-04-10 19:26:54 +0000

    removed unsued code/comments, added a bootstrap table and a filter function that now works after timeout

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,290 +1,67 @@
-//used from code institute's example from online vidoes
-const baseURL = "https://api.coingecko.com/api/v3/exchanges/binance/tickers";
-
-
-
-
-//how we are getting the API data 
-function getData(cb) {
-    let xhr = new XMLHttpRequest();
-
-    xhr.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            cb(JSON.parse(this.responseText));
-        }
-    };
-
-    xhr.open("GET", baseURL);
-    xhr.send();
-}
-
-//creating a table?
-function getTableHeaders(obj) {
-    let tableHeaders = [];
-
-     Object.keys(obj).forEach(function(key){
-        tableHeaders.push(`<td> ${key} </td>`);
-     });
-    return `<tr>${tableHeaders}</tr>`;
-};
-
-// function generatePaginationButtons(next, previous) {
-//     if (next && prev) {
-//         return `<button onclick="writeToDocument(${prev})">Previous</button>`
-//                `<button onclick="writeToDocument(${next})">Next</button>`
-//     } else if (next && !prev) {
-//         `<button onclick="writeToDocument(${next})">Next</button>`
-//     } else if (!next && prev) {
-//         `<button onclick="writeToDocument(${prev})">Previous</button>`
-//     }
-// }
-
-//or is this creating a table?
-function writeToDocument() {
-    let tableRows = [];
-
-    //clears the text
-    let el = document.getElementById('test');
-    el.innerHTML = "";
-    
-// anonymous function (the function in the paramater)
-    getData(function(data) {
-        // let pagination;
-        // if (data.next || data.previous) {
-        //     pagination = generatePaginationButtons(data.next, data.previous)
-        // }
-
-        data = data.tickers;
-        let tableHeaders = getTableHeaders(data[0]);
-
-        data.forEach(function(item) {
-            let dataRow = [];
-
-            Object.keys(item).forEach(function(key){
-                dataRow.push(`<td>${item[key]}</td>`)
-            });
-            tableRows.push(`<tr>${dataRow}</tr>`);
-        });
-        el.innerHTML = `<table> ${tableHeaders}${tableRows} </table>`
-    });
-}
-
-//Filter ATTEMPT 3
-function filterUSDT(obj) {
-    let USDTCoins = obj.tickers.filter( item => item.target === 'USDT'  );
-    return USDTCoins;
-};
-
-function printUSDT(exch) {
-    console.log( filterUSDT(coins[exch]) );
-}
-
-//console.log(USDTCoins);
-
-
-//Filter ATTEMPT 2
-// Object.filter = function( coins, USDT) {
-//     let USDTCoins = {}, key;
-
-//     for (key in obj) {
-//         if (obj.hasOwnProperty(key) && !USDT(obj[key])) {
-//             USDTCoins[key] = obj[key];
-//         }
-//     }
-
-//     console.log(USDTCoins);
-// };
-
-//filter ATEMPT 1
-// let USDTCoins = coins.binance.filter(coin => coin === 'USDT');
-// console.log(USDTCoins);
-
-
-
-//d
+//created a constant with the api URL and variables with desired exchanges
 const exchURL = "https://api.coingecko.com/api/v3/exchanges/";
-let coins = { binance: [], gdax: [] };
+let coins = { binance: [], gdax: [], kraken: [], hotbit: [] };
 
+//looping through all of the pages code to show information, (hardcoded)
 function getData2(exch) {
+    coins[exch] = [];
+    for (let i = 1; i < 3; i++) {
+        const url = exchURL + exch + `/tickers?page=${i}`;
+        processRequest(exch, url);
+    }
+}
+
+// caller 
+function processRequest(exch, url){
     let xhr = new XMLHttpRequest();
 
     xhr.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            coins[exch] = JSON.parse(this.responseText);
+            coins[exch] = coins[exch].concat(JSON.parse(this.responseText).tickers);
             arrivedData();
-            printUSDT(exch);
         }
     };
 
-    xhr.open("GET", exchURL + exch + '/tickers');
+    xhr.open("GET", url);
     xhr.send();
 }
 
 // let arrivedData = function () {} // same as below
 function arrivedData() {
     let result = '';
-
     for (const exch in coins) {
         // if (!exch.hasOwnProperty('tickers')) {
         //     continue;
         // }
-
-        let tickers = coins[exch].tickers;
-
+        // K - because we made the data into an array rather then a dictionary earlier we removed .tickers (which would have given us this array)
+        let tickers = coins[exch];
         for (let i in tickers) {
-            result += `<tr><td>${exch}</td> <td>${tickers[i].base}</td> <td>${tickers[i].last}</td></tr>`;
+            result += `<tr><td>${exch}</td> <td>${tickers[i].base}</td> <td>${tickers[i].target}</td><td>${tickers[i].last}</td><td>${tickers[i].volume}</td></tr>`;
             //result = result +  // the same
         }
     }
     //console.log(result); 
-
     let el = document.getElementById('test');
-    el.innerHTML = `<table>${result}</table>`;
-
+    el.innerHTML = `<table class='table table-dark'>${result}</table>`;
 }
 
+//Filter 
+
+// if (coins['binance'] !== []) {
+//     function filterUSDT(obj) {
+//         let USDTCoins = obj.filter( item => item.target === 'USDT'  );
+//         return USDTCoins;
+//         console.log(filterUSDT(coins['binance']));
+//     };
+// }
 
 
-/*  
-//3. WORKING DISPLAYING THE DATA CORRECTLY IN A TABLE
-const baseURL = "https://api.coingecko.com/api/v3/exchanges/binance/tickers";
-
-function getData(cb) {
-    var xhr = new XMLHttpRequest();
-
-    xhr.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            cb(JSON.parse(this.responseText));
-        }
-    };
-
-    xhr.open("GET", baseURL);
-    xhr.send();
-}
-
-function getTableHeaders(obj) {
-    let tableHeaders = [];
-
-     Object.keys(obj).forEach(function(key){
-        tableHeaders.push(`<td> ${key} </td>`);
-     });
-    return `<tr>${tableHeaders}</tr>`;
+function filterUSDT(obj) {
+    let USDTCoins = obj.filter( item => item.target === 'USDT');
+    return USDTCoins;
+    
 };
 
 
-
-function writeToDocument() {
-    let tableRows = [];
-
-    //clears the text
-    let el = document.getElementById('test');
-    el.innerHTML = "";
-    
-
-    getData(function(data) {
-        data = data.tickers;
-        let tableHeaders = getTableHeaders(data[0]);
-
-        data.forEach(function(item) {
-            let dataRow = [];
-
-            Object.keys(item).forEach(function(key){
-                dataRow.push(`<td>${item[key]}</td>`)
-            });
-            tableRows.push(`<tr>${dataRow}</tr>`);
-        });
-        el.innerHTML = `<table> ${tableHeaders}${tableRows} </table>`
-    });
-}
-*/
-
-/* 
-//2. WORKING CODE DISPLAYS THE KEYS OF EACH OF OBJECT FROM JSON
-const baseURL = "https://api.coingecko.com/api/v3/exchanges/binance/tickers";
-
-function getData(cb) {
-    var xhr = new XMLHttpRequest();
-
-    xhr.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            cb(JSON.parse(this.responseText));
-        }
-    };
-
-    xhr.open("GET", baseURL);
-    xhr.send();
-}
-
-function getTableHeaders(obj) {
-    let tableHeaders = [];
-
-     Object.keys(obj).forEach(function(key){
-        tableHeaders.push(`<td> ${key} </td>`);
-     });
-    return `<tr>${tableHeaders}</tr>`;
-};
-
-
-
-function writeToDocument() {
-    //clears the text
-    let el = document.getElementById('test');
-    el.innerHTML = "";
-    
-
-    getData(function(data) {
-        data = data.tickers;
-        let tableHeaders = getTableHeaders(data[0]);
-
-        data.forEach(function(item) {
-            //el.innerHTML += "<p>" + item.base + "<p>";
-        })
-        el.innerHTML = `<table> ${tableHeaders} </table>`
-    });
-}
-*/
-
-
-/*
-//1. WORKING CODE - PRINTS TO CONSOLE (OUR JSON DATA AS AN OBJECT)
-function getData(cb) {
-    var xhr = new XMLHttpRequest();
-
-    xhr.open("GET", "https://api.coingecko.com/api/v3/exchanges/binance/tickers");
-    xhr.send();
-
-    xhr.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            cb(JSON.parse(this.responseText));
-        }
-    };
-}
-
-function printDataToConsole(data) {
-    console.log(data);
-}
-
-getData(printDataToConsole);
-
-*/
-
-
-/*
-const baseURL = "https://api.coingecko.com/api/v3/exchanges/binance/tickers";
-
-function getData(type, cb) {
-    //what does the new mean and XMLH...request?
-    let xhr = new XMLHttpRequest();
-
-    xhr.open("GET", baseURL  + "/" + type + "/");
-    xhr.send();
-
-//this in this instance mean?
-    xhr.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            cb(JSON.parse(this.responseText));
-        }
-    };
-}
-
-*/
+// setTimeout( ()=> filterUSDT(coins['binance']), 5000);
+//console.log(filterUSDT(coins['binance']));
