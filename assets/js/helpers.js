fd22abd3bbbc29ef29c36e6786d98d4126f64239
wheresyura <yurakovabusiness@gmail.com>
2021-04-18 21:32:06 +0000

    added a filter to filter by volumbeByPrice

--- assets/js/helpers.js
+++ assets/js/helpers.js
@@ -13,13 +13,35 @@ function refreshTickers(){
 function filterData() {
     let exchange = document.getElementById('exchange').value;
     let currency = document.getElementById('currency').value;
+
     
     let priceRange = document.getElementById('price').value.split('-');
     // first part after question is true condition (to execute) and after : is the false part to (execute if is not undefined) 
     let minPrice = priceRange[0] === "" ? 0 : Number(priceRange[0]);
     let maxPrice = priceRange[1] === "" ? Number.MAX_VALUE : Number(priceRange[1]);
 
-    filtered = coins[exchange].filter(ticker => ticker.target === currency && ticker.last >= minPrice && ticker.last <= maxPrice);
+   // volume
+    let volumeByPriceRange = document.getElementById('volumeByPrice').value.split('-');
+    
+    let arrVolume = coins[exchange].map(x=> x.target === currency ? x.volume * x.last : 0) ;
+    let maxVolumeAllCoins = Math.max(...arrVolume ) + 1;
+   
+    let minVolume =  Number(volumeByPriceRange[0]) / 100 * maxVolumeAllCoins;
+    let maxVolume =  Number(volumeByPriceRange[1]) / 100 * maxVolumeAllCoins;
+
+
+
+
+    filtered = coins[exchange].filter(
+        ticker => 
+            ticker.target === currency 
+            && ticker.last >= minPrice 
+            && ticker.last <= maxPrice 
+            && ticker.volume * ticker.last  >= minVolume
+            && ticker.volume * ticker.last  <= maxVolume      
+        );
+
+ 
 
     // for (let i = 0; i < filtered.length; i++) {
     //     filtered[i]['marketCap'] = i;
